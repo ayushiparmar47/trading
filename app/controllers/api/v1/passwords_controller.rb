e02fd33b14935556f5f7f9821136fb9630dc06b8
Ayushi Parmar <ayushi.parmar@protonshub.in>
2020-11-23 11:50:50 +0530

    002 : Removed debugger

--- app/controllers/api/v1/passwords_controller.rb
+++ app/controllers/api/v1/passwords_controller.rb
@@ -1,35 +1,32 @@
 # frozen_string_literal: true
 
 class Api::V1::PasswordsController < Devise::PasswordsController
-  before_action :check_valid_token ,only: [:edit]
+  # before_action :check_valid_token ,only: [:edit]
   # GET /resource/password/new
   def new
-    super
-    debugger
+    # super
   end
 
   # POST /resource/password
   def create
     # super
-    debugger
-    @user = User.find_by_email(params[:user][:email])
-    if @user.present?
-     @user.send_reset_password_instructions
-     render json: {message: "updated"}
-    else
-        render json: {message: "no such email is present"}
-    end
+    # @user = User.find_by_email(params[:user][:email])
+    # if @user.present?
+    #  @user.send_reset_password_instructions
+    #  render json: {message: "updated"}
+    # else
+    #     render json: {message: "no such email is present"}
+    # end
   end
 
   # GET /resource/password/edit?reset_password_token=abcdef
   def edit
-    debugger
-    user = User.find_by_email(params[:email])
-    if user.present? && @flag
+    # user = User.find_by_email(params[:email])
+    # if user.present? && @flag
       
-    else
+    # else
 
-    end
+    # end
   end
 
 
@@ -57,27 +54,26 @@ class Api::V1::PasswordsController < Devise::PasswordsController
   end
   # PUT /resource/password
   def update
-    super
+    # super
   end
   
   private
 
   def check_valid_token
-    debugger
-    @user = User.find_by!(reset_password_token: params[:reset_password_token])
-    @flag = true if @user.present?
-    rescue ActiveRecord::RecordNotFound
-      @flag = false
+    # @user = User.find_by!(reset_password_token: params[:reset_password_token])
+    # @flag = true if @user.present?
+    # rescue ActiveRecord::RecordNotFound
+    #   @flag = false
       # render json: {message: "Inavlid token"}
   end
-  protected
+  # protected
 
-  def after_resetting_password_path_for(resource)
-    super(resource)
-  end
+  # def after_resetting_password_path_for(resource)
+  #   super(resource)
+  # end
 
   # The path used after sending reset password instructions
-  def after_sending_reset_password_instructions_path_for(resource_name)
-    super(resource_name)
-  end
+  # def after_sending_reset_password_instructions_path_for(resource_name)
+  #   super(resource_name)
+  # end
 end
