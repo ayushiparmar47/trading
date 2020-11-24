8c1e507a46fc77573e7bb4aa7a66a030e0975b3d
Ayushi Parmar <ayushi.parmar@protonshub.in>
2020-11-24 18:32:05 +0530

    009:email seetings

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+Rails.application.configure do
+	config.action_mailer.default_url_options = { :host => 'desolate-ravine-19733.herokuapp.com' }
+end
