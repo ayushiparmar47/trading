d4727f4d72f920adaa9c51f6bf1646b3842ba19c
Ayushi Parmar <ayushi.parmar@protonshub.in>
2020-11-24 18:43:50 +0530

    0010:email settings in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,4 +82,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { :host => "desolate-ravine-19733.herokuapp.com" }
 end
