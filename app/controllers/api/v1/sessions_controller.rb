41dc572595c35f7b8040fbdd4227177b02fa7350
Ayushi Parmar <ayushi.parmar@protonshub.in>
2020-11-26 16:23:17 +0530

    0016: Json parse for sign in

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -11,13 +11,14 @@ class Api::V1::SessionsController < Devise::SessionsController
 
   # POST /api/v1/users/sign_in
   def create
+    params[:user] = JSON.parse(params[:user])
     email = params[:user][:email]
     if User.find_by_email(email)
       user = User.find_by_email(email)
       if user.present? && user.valid_password?(params[:user][:password])
         # user = warden.authenticate!(:scope => :user)
         token = Tiddle.create_and_return_token(user, request)
-        render json: {success: true, message: "User Successfully Signed in ",authentication_token: token}
+        render json: {success: true, message: "User Successfully Signed in ",user: user.as_json.merge({token: token})}
       else
         render json: {success: false, message: "Email or Password is Invalid"}, status: 404
       end
