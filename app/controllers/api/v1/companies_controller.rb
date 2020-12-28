9bc5846672c402dc874bcee51eeddf61ea3ca857
Ayushi Parmar <ayushi.parmar@protonshub.in>
2020-12-28 12:43:17 +0530

    Response Format changes

--- app/controllers/api/v1/companies_controller.rb
+++ app/controllers/api/v1/companies_controller.rb
@@ -6,23 +6,27 @@ class Api::V1::CompaniesController < ApplicationController
 	# get /api/v1/get_todays_trades
 	def get_todays_trades
 		if current_api_v1_user.present?
-			if current_api_v1_user.plans.first.name == "free"
-				@trades = TodayTrade.where(for_free_plan: true)
-				@free_trades = fetch_trades @trades
-				if @free_trades.present?
-					render json: {success: true, data: @free_trades.as_json}, status: 200
+			if current_api_v1_user.plans.present?
+				if current_api_v1_user.plans.first.name == "free"
+					@trades = TodayTrade.where(for_free_plan: true)
+					@free_trades = fetch_trades @trades
+					if @free_trades.present?
+						render json: {success: true, data: @free_trades.as_json}, status: 200
+					else
+						render json: {success: false, message: "No trades suggestion is present"}
+					end
 				else
-					render json: {success: false, message: "No trades suggestion is present"}
+				# ALL TRADES WILL BE SHOWN FOR PREMIUM
+					@p_trades = TodayTrade.all
+					@paid_trades = fetch_trades @p_trades
+					if @paid_trades.present?
+						render json: {success: true, data: @paid_trades.as_json}, status: 200
+					else
+						render json: {success: false, message: "No trades suggestion is present"}
+					end
 				end
 			else
-			# RIGHT NOW ALL TRADES WILL BE SHOWN
-				@p_trades = TodayTrade.all
-				@paid_trades = fetch_trades @p_trades
-				if @paid_trades.present?
-					render json: {success: true, data: @paid_trades.as_json}, status: 200
-				else
-					render json: {success: false, message: "No trades suggestion is present"}
-				end
+				render json: {success: false, message: "Please Subscribe to our plans"}
 			end
 		else
 			render json: {success: false, message: "Please Sign in first.."}
@@ -42,9 +46,10 @@ class Api::V1::CompaniesController < ApplicationController
 			expected_rate = symbol[1]
 			difference , percenage_difference = fetch_gap expected_rate, current_rate
 			data = {logo: company_details["logo"],comapany_name: symbol[2],symbol: symbol[0],gap_in_percent: percenage_difference,gap: difference,current_rate: current_rate,expected_rate: symbol[1],company_profile: { company_details: company_details, company_id: symbol[3], trade_id: symbol[4], company_history_rate: company_history_rate}}
-			data_hash["trade_data_#{i+1}"] = data 
+			# data_hash["trade_data_#{i+1}"] = data
+			data_array << data 
 		end
-			data_array << data_hash
+			# data_array << data_hash
 		return data_array
 	end
 
