48b7b2d04a98dd705067f9a2395d7edbe7329729
Ayushi Parmar <ayushi.parmar@protonshub.in>
2020-12-28 12:03:44 +0530

    Access Token updated

--- app/controllers/api/v1/companies_controller.rb
+++ app/controllers/api/v1/companies_controller.rb
@@ -6,26 +6,24 @@ class Api::V1::CompaniesController < ApplicationController
 	# get /api/v1/get_todays_trades
 	def get_todays_trades
 		if current_api_v1_user.present?
-			# trade_data = fetch_trades
-
-		# if current_api_v1_user.plan == "free"
-			# @trades = TodayTrade.where(for_free_plan: true)
-			# @free_trades = fetch_trades @trades
-			# if @free_trades.present?
-			# 	render json: {success: true, data: @free_trades.as_json}, status: 200
-			# else
-			# 	render json: {success: false, message: "No trades suggestion is present"}
-			# end
-		# else
-		# RIGHT NOW ALL TRADES WILL BE SHOWN
-			@p_trades = TodayTrade.all
-			@paid_trades = fetch_trades @p_trades
-			if @paid_trades.present?
-				render json: {success: true, data: @paid_trades.as_json}, status: 200
+			if current_api_v1_user.plans.first.name == "free"
+				@trades = TodayTrade.where(for_free_plan: true)
+				@free_trades = fetch_trades @trades
+				if @free_trades.present?
+					render json: {success: true, data: @free_trades.as_json}, status: 200
+				else
+					render json: {success: false, message: "No trades suggestion is present"}
+				end
 			else
-				render json: {success: false, message: "No trades suggestion is present"}
+			# RIGHT NOW ALL TRADES WILL BE SHOWN
+				@p_trades = TodayTrade.all
+				@paid_trades = fetch_trades @p_trades
+				if @paid_trades.present?
+					render json: {success: true, data: @paid_trades.as_json}, status: 200
+				else
+					render json: {success: false, message: "No trades suggestion is present"}
+				end
 			end
-		# end
 		else
 			render json: {success: false, message: "Please Sign in first.."}
 		end
@@ -34,6 +32,7 @@ class Api::V1::CompaniesController < ApplicationController
 	# Fetch the trades for the selected companies
 	def fetch_trades trades
 		symbols = []
+		data_array = []
 		data_hash = {}
 		trades.select { |t| symbols << [t.company.symbol,t.company.expected_rate,t.company.name,t.company.id,t.id]}
 		symbols.each_with_index do |symbol,i|
@@ -45,7 +44,8 @@ class Api::V1::CompaniesController < ApplicationController
 			data = {logo: company_details["logo"],comapany_name: symbol[2],symbol: symbol[0],gap_in_percent: percenage_difference,gap: difference,current_rate: current_rate,expected_rate: symbol[1],company_profile: { company_details: company_details, company_id: symbol[3], trade_id: symbol[4], company_history_rate: company_history_rate}}
 			data_hash["trade_data_#{i+1}"] = data 
 		end
-		return data_hash
+			data_array << data_hash
+		return data_array
 	end
 
 	# Calculate the difference and percenage difference
