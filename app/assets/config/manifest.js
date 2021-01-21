999ae350b2e6245037ceef30b586e85b03b71eca
Ayushi Parmar <ayushi.parmar@protonshub.in>
2021-01-21 13:57:21 +0530

    add html_safe

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,1 +1,5 @@
 //= link blanck_user.png
+//= link ckeditor/filebrowser/thumbs/unknown.gif
+//= link ckeditor/filebrowser/thumbs/jpg.gif
+//= link ckeditor/application.css
+//= link ckeditor/application.js
