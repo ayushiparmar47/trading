044d7e0a6f2d346c81d4550ccd095856f9139033
Ayushi Parmar <ayushi.parmar@protonshub.in>
2021-02-10 12:50:16 +0530

    screte_key of stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,2 +1,8 @@
-Stripe.api_key = "tGaNWsIG3Qy6zvXB8wv4rEcizJpSXjF4"
-STRIPE_PUBLIC_KEY = "pk_KcSyS2qPWdT5SdrwkQg0vNSyhZgqP"
+# Stripe.api_key = "tGaNWsIG3Qy6zvXB8wv4rEcizJpSXjF4"
+# STRIPE_PUBLIC_KEY = "pk_KcSyS2qPWdT5SdrwkQg0vNSyhZgqP"
+
+# Stripe.api_key = "sk_test_51IH6KdJRFx4DxwrmQ4BnnLFlfftsBTEAA35wQ8L3sN0MsaiDp1vyaff4xsztvtrC3BMPY1v2dfoH6nVgjrVIF0OB00X9YTtGfw"
+# STRIPE_PUBLIC_KEY = "pk_test_51IH6KdJRFx4DxwrmmEiyABJA77tiFqpvwXEjxZk53eFKf6lI6MF0Ao4UNFeecVz0YhPBtoYQHoeFq7fLDVK8wLzQ00UBGwYnRk"
+
+Stripe.api_key = "sk_test_51HvlOoLCsvC7SSERuONAQ3uJYbjABGwY5TTN8LykF8vm0oduMNwBfVlwkPRU27qpHAiyxHa98McuW3b4FLGDmZdY005VCNBA7u"
+STRIPE_PUBLIC_KEY = "pk_test_51HvlOoLCsvC7SSERJUqyazq33312eMMHdVc2ka4L4kDqrrdzC9CMDYntveCcEt202DS0VrUQzLdQpBTf6VkjKOi800R9JWZiPc"
