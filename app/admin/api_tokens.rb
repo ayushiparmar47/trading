60ea187d95afdb0b9141d9905ec94b039a431a58
Ayushi Parmar <ayushi.parmar@protonshub.in>
2021-03-03 17:39:29 +0530

    Hide new button

--- app/admin/api_tokens.rb
+++ app/admin/api_tokens.rb
@@ -1,18 +1,8 @@
 ActiveAdmin.register ApiToken do
   permit_params :finnhub_api_token, :iexcloud_api_token
-  actions :all, :except => [:destroy]
-  
-  controller do 
-    def create
-      @api_tokens = ApiToken.first || ApiToken.new
-      @api_tokens.finnhub_api_token = params[:api_token][:finnhub_api_token]
-      @api_tokens.iexcloud_api_token = params[:api_token][:iexcloud_api_token]
-      if @api_tokens.save
-        flash[:notice] = "Saved sucessfully."
-      else
-        flash[:notice] = "Something went wrong :  #{@api_tokens.errors.full_messages}"
-      end
-      redirect_to admin_api_tokens_path
-    end
+  if ApiToken.count == 1
+    actions :all, :except => [:destroy,:new]
+  else
+    actions :all, :except => [:destroy]
   end
 end
