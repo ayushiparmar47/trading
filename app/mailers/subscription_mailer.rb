bde4b010c9ad4d1fd2ac966b34589f882a211101
Ayushi Parmar <ayushi.parmar@protonshub.in>
2021-03-15 17:29:58 +0530

    Change in subject

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -3,11 +3,11 @@ class SubscriptionMailer < ApplicationMailer
   	@user = subscription.user
   	@plan = subscription.plan
   	@subscription = subscription
-    mail to: @user.email, subject: "plan subscription"
+    mail to: @user.email, subject: "Your plan is about to expier!"
   end
 
   def expire_mail(user)
   	@user = user
-  	mail to: @user.email, subject: "plan Expired"
+  	mail to: @user.email, subject: "Your plan is expiered!"
   end
 end
