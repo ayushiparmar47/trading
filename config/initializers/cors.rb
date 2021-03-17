bd06f7cfe22e057ba56a345b777301be21924a5e
Ayushi Parmar <ayushi.parmar@protonshub.in>
2021-03-17 16:46:32 +0530

    Cors error fixed

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://m-work-5e18a.web.app'
 
     resource '*',
       headers: :any,
